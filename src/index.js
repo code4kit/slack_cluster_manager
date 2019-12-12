eb0f99ed8a44c7ce547ce3ff24778bdebbb9e0ba
Vireakpanha Khy <48462005+vireakpanha@users.noreply.github.com>
2019-12-13 06:25:49 +0700

    Fixed "semistandarad" errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * fileOverView index.js
@@ -10,14 +10,14 @@
  * @version 1.0.0
  */
 
-require("dotenv").config();
-const http = require("http");
-const httpServer = require("./lib/http_server");
-const { WebClient, RTMClient } = require("@slack/client");
-const cluster = require("./lib/model/cluster");
-const packageInfo = require("../package.json");
-const CronJob = require("cron").CronJob;
-const fetch = require("fetch").fetchUrl;
+require('dotenv').config();
+const http = require('http');
+const httpServer = require('./lib/http_server');
+const { WebClient, RTMClient } = require('@slack/client');
+const cluster = require('./lib/model/cluster');
+const packageInfo = require('../package.json');
+const CronJob = require('cron').CronJob;
+const fetch = require('fetch').fetchUrl;
 
 // Environment Variables
 const PORT = process.env.PORT;
@@ -39,43 +39,43 @@ const webClientLegacy = new WebClient(LEGACY_TOKEN);
 /**
  * @param {Object} event
  */
-const nedb = require("./lib/model/_nedb")(DB_PATH, SYSTEM_MODE);
+const nedb = require('./lib/model/_nedb')(DB_PATH, SYSTEM_MODE);
 
 /**
  * @type {{String: RegExp}}
  */
 const slackMsgRegExp = {
   update: new RegExp(`^${EMOJI} update (.+)`),
-  mention: new RegExp(`^${EMOJI} (mention [A-Z]|[0-9])`, "i"),
+  mention: new RegExp(`^${EMOJI} (mention [A-Z]|[0-9])`, 'i'),
   invite: new RegExp(`^${EMOJI} invite (.+)`),
-  kick: new RegExp(`^${EMOJI} (kick [A-Z]|[0-9])`, "i"),
-  list: new RegExp(`^${EMOJI} (list [A-Z]|[0-9])|list$`, "i")
+  kick: new RegExp(`^${EMOJI} (kick [A-Z]|[0-9])`, 'i'),
+  list: new RegExp(`^${EMOJI} (list [A-Z]|[0-9])|list$`, 'i')
 };
 
-rtmClient.on("message", event => {
-  if (event.user === SLACK_BOT_ID || !("text" in event)) {
+rtmClient.on('message', event => {
+  if (event.user === SLACK_BOT_ID || !('text' in event)) {
     return;
   }
   if (event.text.match(slackMsgRegExp.update)) {
-    console.log("do updateCmd");
+    console.log('do updateCmd');
     updateCmd(event);
-    console.log("done updateCmd");
+    console.log('done updateCmd');
   } else if (event.text.match(slackMsgRegExp.mention)) {
-    console.log("do mentionCmd");
+    console.log('do mentionCmd');
     mentionCmd(event);
-    console.log("done mentionCmd");
+    console.log('done mentionCmd');
   } else if (event.text.match(slackMsgRegExp.invite)) {
-    console.log("do inviteCmd");
+    console.log('do inviteCmd');
     inviteCmd(event);
-    console.log("done inviteCmd");
+    console.log('done inviteCmd');
   } else if (event.text.match(slackMsgRegExp.kick)) {
-    console.log("do kickCmd");
+    console.log('do kickCmd');
     kickCmd(event);
-    console.log("done kickCmd");
+    console.log('done kickCmd');
   } else if (event.text.match(slackMsgRegExp.list)) {
-    console.log("do listCmd");
+    console.log('do listCmd');
     listCmd(event);
-    console.log("done listCmd");
+    console.log('done listCmd');
   }
 });
 
@@ -91,12 +91,12 @@ const updateCmd = async slackEvent => {
     const args = slackEvent.text.trim().split(/update|<@|>/);
     const gotClusterName = args[1].trim();
     const membersArray = args.filter(arg => {
-      return arg[0] === "U";
+      return arg[0] === 'U';
     });
 
-    let isVirtualCompany = slackEvent.text.indexOf(" --vc") != -1;
+    const isVirtualCompany = slackEvent.text.indexOf(' --vc') !== -1;
 
-    if (gotClusterName.match(new RegExp("^[a-z0-9-_]+$"))) {
+    if (gotClusterName.match(new RegExp('^[a-z0-9-_]+$'))) {
       const resultOfUpdated = await cluster.update(
         nedb,
         gotClusterName,
@@ -130,7 +130,7 @@ const mentionCmd = async slackEvent => {
   const clusterName = slackEvent.text
     .split(/mention/i)[1]
     .trim()
-    .split(" ")[0]
+    .split(' ')[0]
     .trim();
   const msgForSending = slackEvent.text
     .split(clusterName)[1]
@@ -142,7 +142,7 @@ const mentionCmd = async slackEvent => {
 
   if (targetCluster) {
     if (msgForSending) {
-      for (memberId of targetCluster.members) {
+      for (const memberId of targetCluster.members) {
         const message = `${msgForSending} https://${SLACK_WORKSPACE}.slack.com/archives/${slackEvent.channel}/p${slackEvent.event_ts}`;
         directMessage(memberId, message);
       }
@@ -150,11 +150,11 @@ const mentionCmd = async slackEvent => {
       replyToThread(
         slackEvent.channel,
         slackEvent.ts,
-        "Please provide the message you want to send.\n\n"
+        'Please provide the message you want to send.\n\n'
       );
     }
   } else {
-    const msg = "Please make sure the cluster name is correct!!.";
+    const msg = 'Please make sure the cluster name is correct!!.';
     replyToThread(slackEvent.channel, slackEvent.ts, msg);
   }
 };
@@ -170,7 +170,7 @@ const inviteCmd = async slackEvent => {
     return replyToThread(
       slackEvent.channel,
       slackEvent.ts,
-      "This cluster is not found"
+      'This cluster is not found'
     );
   }
   const existingMember = await webClient.conversations.members({
@@ -181,10 +181,10 @@ const inviteCmd = async slackEvent => {
     if (!existingMember.members.includes(member)) return member;
   });
   await webClientLegacy.conversations.invite({
-    users: inviteMembers.join(","),
+    users: inviteMembers.join(','),
     channel: slackEvent.channel
   });
-  replyToThread(slackEvent.channel, slackEvent.ts, "Invitation Complete!");
+  replyToThread(slackEvent.channel, slackEvent.ts, 'Invitation Complete!');
 };
 
 /**
@@ -195,32 +195,32 @@ const kickCmd = async slackEvent => {
   const gotClusterName = slackEvent.text
     .split(/kick/i)[1]
     .trim()
-    .split(" ")[0]
+    .split(' ')[0]
     .trim();
   const targetCluster = await cluster.findMembers(nedb, gotClusterName);
-  const channel_name = await getChannelName(slackEvent);
+  const channelName = await getChannelName(slackEvent);
   if (!targetCluster) {
     replyToThread(
       slackEvent.channel,
       slackEvent.ts,
-      "This cluster is not found"
+      'This cluster is not found'
     );
   } else {
     const channel = await webClient.conversations.members({
       channel: slackEvent.channel
     });
-    for (member of targetCluster.members) {
+    for (const member of targetCluster.members) {
       if (channel.members.includes(member)) {
         kickMembers(member, slackEvent);
       }
     }
-    const message = `<@${slackEvent.user}> kicked ${gotClusterName} from ${channel_name} https://${SLACK_WORKSPACE}.slack.com/archives/${slackEvent.channel}/p${slackEvent.event_ts}`;
+    const message = `<@${slackEvent.user}> kicked ${gotClusterName} from ${channelName} https://${SLACK_WORKSPACE}.slack.com/archives/${slackEvent.channel}/p${slackEvent.event_ts}`;
     directMessage(SLACK_USER_ID, message);
   }
 };
 
 const kickMembers = async (memberId, slackEvent) => {
-  const channel_name = await getChannelName(slackEvent);
+  const channelName = await getChannelName(slackEvent);
   if (memberId !== slackEvent.user) {
     await webClient.conversations
       .kick({
@@ -228,7 +228,7 @@ const kickMembers = async (memberId, slackEvent) => {
         user: memberId
       })
       .then(async () => {
-        const message = `<@${slackEvent.user}> kicked you from ${channel_name}`;
+        const message = `<@${slackEvent.user}> kicked you from ${channelName}`;
         directMessage(memberId, message);
       })
       .catch(err => console.log(err));
@@ -242,10 +242,10 @@ const kickMembers = async (memberId, slackEvent) => {
 };
 
 const getChannelName = async slackEvent => {
-  let channel_name = await webClient.channels.info({
+  const channelName = await webClient.channels.info({
     channel: slackEvent.channel
   });
-  return channel_name.channel.name;
+  return channelName.channel.name;
 };
 
 const directMessage = async (userId, msg) => {
@@ -267,20 +267,20 @@ const listCmd = async slackEvent => {
   const gotClusterName = slackEvent.text
     .split(/list/i)[1]
     .trim()
-    .split(" ")[0]
+    .split(' ')[0]
     .trim();
-  if (slackEvent.channel[0] !== "D") {
-    replyToThread(slackEvent.channel, slackEvent.ts, "This cmd only DM.");
+  if (slackEvent.channel[0] !== 'D') {
+    replyToThread(slackEvent.channel, slackEvent.ts, 'This cmd only DM.');
     return;
   }
   let infoAllCluster = await cluster.find(nedb, gotClusterName);
   console.log(infoAllCluster);
   if (!infoAllCluster) {
-    replyToThread(slackEvent.channel, slackEvent.ts, "Nothing to return");
+    replyToThread(slackEvent.channel, slackEvent.ts, 'Nothing to return');
   } else {
-    let msg = "";
+    let msg = '';
     infoAllCluster = gotClusterName ? [infoAllCluster] : infoAllCluster;
-    for (data of infoAllCluster) {
+    for (const data of infoAllCluster) {
       const members = data.members.map(member => `<@${member}>`);
       msg += `:file_folder: Cluster_name ${data.cluster_name}\n\n Members:sunglasses: ${members}\n`;
     }
@@ -309,9 +309,9 @@ const replyToThread = (ch, ts, msg) => {
  * @param {Object} res
  */
 const URLRegExp = {
-  batch: new RegExp("^/batch/b-[0-9]+$", "i"),
-  student: new RegExp("^/student/[A-Z0-9]{9}$", "i"),
-  virtual_company: new RegExp("^/virtual_company/[A-Z0-9]+$", "i")
+  batch: new RegExp('^/batch/b-[0-9]+$', 'i'),
+  student: new RegExp('^/student/[A-Z0-9]{9}$', 'i'),
+  virtual_company: new RegExp('^/virtual_company/[A-Z0-9]+$', 'i')
 };
 
 const server = http.createServer((req, res) => {
@@ -319,60 +319,64 @@ const server = http.createServer((req, res) => {
 
   console.log(req.url.substr(9));
 
-  if (req.url == "/") {
-    res.write("server created");
+  if (req.url === '/') {
+    res.write('server created');
     res.end();
   } else if (req.url.match(URLRegExp.student)) {
-    let user_property = {
-      batch: "",
-      virtual_company: ""
+    const userProperty = {
+      batch: '',
+      virtual_company: ''
     };
 
     nedb.find({}, (err, data) => {
+      if (err) console.log(err);
+
       data.forEach(cluster => {
         if (cluster.members.includes(req.url.substr(9))) {
           console.log(cluster.is_virtual_company);
           if (cluster.is_virtual_company) {
-            user_property.virtual_company = cluster.cluster_name;
-          } else if (cluster.cluster_name.match(new RegExp("^b-[0-9]+$"))) {
-            user_property.batch = cluster.cluster_name;
+            userProperty.virtual_company = cluster.cluster_name;
+          } else if (cluster.cluster_name.match(new RegExp('^b-[0-9]+$'))) {
+            userProperty.batch = cluster.cluster_name;
           }
         }
       });
 
-      res.write(JSON.stringify(user_property));
+      res.write(JSON.stringify(userProperty));
       res.end();
     });
   } else if (
     req.url.match(URLRegExp.batch) ||
     req.url.match(URLRegExp.virtual_company)
   ) {
-    let cluster_property = {
-      name: "",
+    const clusterProperty = {
+      name: '',
       members: []
     };
 
     nedb.find({}, (err, data) => {
+      if (err) console.log(err);
+
       data.forEach(cluster => {
         if (
-          cluster.cluster_name == req.url.substr(7) ||
-          cluster.cluster_name == req.url.substr(17)
+          cluster.cluster_name === req.url.substr(7) ||
+          cluster.cluster_name === req.url.substr(17)
         ) {
           if (
             req.url.match(URLRegExp.batch) ||
             req.url.match(URLRegExp.virtual_company)
           ) {
-            cluster_property.name = cluster.cluster_name;
-            cluster_property.members = cluster.members;
+            clusterProperty.name = cluster.cluster_name;
+            clusterProperty.members = cluster.members;
           }
         }
       });
 
-      res.write(JSON.stringify(cluster_property));
+      res.write(JSON.stringify(clusterProperty));
       res.end();
     });
   } else {
-    res.write("Invalid Request");
+    res.write('Invalid Request');
     res.end();
   }
 });
@@ -380,8 +384,10 @@ server.listen(PORT, () => {
   console.log(`listening to PORT:${PORT}`);
 });
 
-const job = new CronJob("* * * * * *", () => {
-  fetch("https://www.temp-scm.glitch.me", (error, meta, body) => {
+const job = new CronJob('* * * * * *', () => {
+  fetch('https://www.temp-scm.glitch.me', (error, meta, body) => {
+    if (error) console.log(error);
+
     console.log(body);
   });
 });
